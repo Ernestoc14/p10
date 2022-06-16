7737f43c9d2aa72f01fb2137a08fb524311582cb
Ernestoc14 <descreed34@hotmail.com>
2022-06-15 20:21:15 -0500

    Add Comms

---
+++ p10.java
@@ -0,0 +1,18 @@
+// Queremos guardar la temperatura mínima y máxima de los 7 días de la semana y para esto
+// utilice un arreglo bidimensional.
+// Una vez guardados los datos en la matriz el programa debe generar la siguiente información:
+// 1. La temperatura media de cada día
+// 2. Temperatura más baja y los días con esa temperatura
+// 3. Se lee una temperatura por teclado y se muestran los días cuya temperatura máxima
+// coincide con ella. Si no existe ningún día se muestra un mensaje notificando que no
+// hay ningún día con esa temperatura.
+
+public class p10 {
+    // Array Temps para guardar la min y max temp de cada dia de la semana
+    //Generar 1-Prom por dia 2-Temp mas baja y dias con esa temp 
+    //3-Leer un temp y mostrar dias si su tem Max coincide con esa temp, sino hay ningun dia imprimir que no hay dia con esa tem max
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
